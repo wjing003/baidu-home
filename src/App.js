d01e556dc0c8635b8c428cceb03eee7d95250538
靖丰年 <jingfengnian@jingfengniandeMacBook-Pro.local>
2019-02-12 00:25:37 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchCom from './SearchCom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wraper">
+        <div id="head">
+          <div id="lg">
+            <img hidefocus="true" alt='' class="index-logo-srcnew" src="//www.baidu.com/img/bd_logo1.png?qua=high" width="270" height="129" usemap="#mp"/>
+          
+          </div>
+          <SearchCom />
+        </div>
       </div>
     );
   }
